59a987bf53a688741c1ebdf2b1e84b71adc0f16b
Andrew Faust <andrewjfaust@yahoo.com>
2017-01-24 20:37:02 -0800

    Added descriptions to functions

--- project1/fs.py
+++ project1/fs.py
@@ -18,24 +18,35 @@ def length(fd):
 #Returns the current read/write position in the file    
 def pos(fd):
     
+#Sets the read/write position to pos
 def seek(fd, pos):
     
+#returns a string; raises an exception if the read would extend beyond the current length of the file
 def read(fd, nbtes):
     
+#Writes to a file, where writebuf is a string
 def write(fd, writebuf):
     
+#reads the entire file, returning a list of strings; treats any 0xa byte it encounters as end of a line; does NOT change the pos value
 def readlines(fd):
-
+    
+#Deletes a given file
 def delfile(filename):
     
+#Creates a directory named "dirname"
 def mkdir(dirname):
-
+    
+#Deletes a given directory
 def deldir(dirname):
     
-def isdir():
+#Returns true if "dirname" is a directory, false otherwise
+def isdir(dirname):
     
-def listdir():
-
+#Lists all files in directory "dirname"
+def listdir(dirname):
+    
+#Suspends the current file system
 def suspend():
-
+    
+#Resumes the previously suspended file system
 def resume():
