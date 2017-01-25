e18ef60506313901f1a57adf6257b1b08381ba97
Andrew Faust <andrewjfaust@yahoo.com>
2017-01-24 20:30:54 -0800

    Update fs.py
    Added all functions needed

--- project1/fs.py
+++ project1/fs.py
@@ -1,9 +1,41 @@
 import os
 
-class fs:
-    def __init__(self):
+#Initializes file system
+def init(fsname):
+    
+#Creates a file with a size of nbytes
+def create(filename, nbytes):
+    
+#Opens a file with the given mode
+def open(filename, mode):
+    
+#Closes a certain file
+def close(fd):
+    
+#Returns the length (in bytes) of a given file
+def length(fd):
+    
+#Returns the current read/write position in the file    
+def pos(fd):
+    
+def seek(fd, pos):
+    
+def read(fd, nbtes):
+    
+def write(fd, writebuf):
+    
+def readlines(fd):
 
+def delfile(filename):
+    
+def mkdir(dirname):
 
+def deldir(dirname):
+    
+def isdir():
+    
+def listdir():
 
+def suspend():
 
-
+def resume():
