083cba6065851c35f045baa86290a9a3891e7f2a
Ajfaust <frostyflakes59@gmail.com>
2017-02-17 12:50:36 -0800

    Update ProbB.py
    Added initializers for start and end of threads, along with class for threading.

--- Project2/ProbB.py
+++ Project2/ProbB.py
@@ -1,1 +1,24 @@
+import threading
+import os
 
+class threadClass(threading.Thread):
+    array = []
+    def __init__(sByte, eByte):
+        thread.Threading.__init__(self)
+        self.start = sByte
+        self.end = eByte
+
+
+def linelengths(filenm, ntrh):
+    fSize = os.path.getSize(filenm)
+    nextByte = 0
+    for i in range(ntrh):
+        startByte = nextByte
+        if i is not ntrh - 1:
+            endByte = startByte + (fSize/ntrh)
+            nextByte = endByte + 1
+        else:
+            endByte = fSize - 1
+
+        t = threadClass(startByte, endByte)
+        t.start()
