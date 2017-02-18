8c68c4ad73ffa7de03acecc60fba17543fb5e3f9
tihju <tgzhu@ucdavis.edu>
2017-02-18 13:47:28 -0800

    just add a print statment to see how many lines we have in the file

--- Project2/nonthreadcount.py
+++ Project2/nonthreadcount.py
@@ -12,6 +12,8 @@ print resultlist
 resultlistb = ProbB.linelengths('infile.txt',6)
 print resultlistb
 
+print 'number of lines in the file is: ' + str(len(resultlistb))
+
 for i in range(len(resultlist)):
     if resultlist[i] != resultlistb[i]:
         print 'two list are not equal in line: ' + str(i)
