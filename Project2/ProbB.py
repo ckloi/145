47ca073a905daa8dac4e0949be32951f93031c3c
ckloi <ckloi@ucdavis.edu>
2017-02-18 00:51:07 -0800

    implemented our ideas, check it out .

--- Project2/ProbB.py
+++ Project2/ProbB.py
@@ -1,40 +1,52 @@
 import threading
 import os
+import time
 
 class threasdClass(threading.Thread):
     resultList = []
     id = 0
     lastThread = 0
     extraLine = False
+    extraLineLock = threading.Lock()
+    #def __init__(self,sByte, eByte,fd):
     def __init__(self,sbyte,fd,chucksize):
         threading.Thread.__init__(self)
-        #this is for each thread's return list
         self.localList = []
         self.startbyte = sbyte
         self.chucksize = chucksize
+        #self.endbyte = eByte
         self.myid = threasdClass.id
         threasdClass.id += 1
         self.fd = fd
 
     def run(self):
+        #self.fd.seek(self.startbyte)
+        #l = self.fd.read(self.endbyte - self.startbyte + 1)
         l = self.fd.read(self.chucksize)
-        # check end of line for the last char of the chuck
+        # check end of line
         flag = False
         if l.endswith('\n'):
             l = l[:-1]
         # if data is splitted into two chunk
         else:
             flag = True
-        #put every number of chars of each line into the local lsit
+
         self.localList = map(lambda x: len(x),l.split('\n'))
-        if threasdClass.extraLine:
-            threasdClass.resultList[-1] += self.localList.pop(0)
-        threasdClass.resultList.extend(self.localList)
-        threasdClass.lastThread += 1
-        if flag:
-            threasdClass.extraLine = True
-        else:
-            threasdClass.extraLine = False
+        while 1:
+            if threasdClass.lastThread == self.myid:
+                if threasdClass.extraLine:
+                    threasdClass.resultList[-1] += self.localList.pop(0)
+                threasdClass.resultList.extend(self.localList)
+                threasdClass.lastThread += 1
+                threasdClass.extraLineLock.acquire()
+                if flag:
+                    threasdClass.extraLine = True
+                else:
+                    threasdClass.extraLine = False
+                threasdClass.extraLineLock.release()
+                break
+            else:
+                time.sleep(0)
 
 
 
@@ -53,7 +65,6 @@ def linelengths(filenm, ntrh):
         #lastchuck -= startbyte
         if i is (ntrh - 1):
             chucksize = fSize - startbyte
-        print chucksize
         t = threasdClass(startbyte,fd,chucksize)
         t.start()
     return threasdClass.resultList
