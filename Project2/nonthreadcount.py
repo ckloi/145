537946b1523fc304f6fff777b662d81f9165d084
tihju <tgzhu@ucdavis.edu>
2017-02-18 13:45:23 -0800

    i add a sort of a test scrip to test if the thread and the non-thread approaches will have a same result. and it turns out that there is a problem.

---
+++ Project2/nonthreadcount.py
@@ -0,0 +1,18 @@
+import ProbB
+
+f = open('infile.txt')
+
+resultlist = []
+
+for l in f:
+    resultlist.append(len(l[:-1]))
+
+print resultlist
+
+resultlistb = ProbB.linelengths('infile.txt',6)
+print resultlistb
+
+for i in range(len(resultlist)):
+    if resultlist[i] != resultlistb[i]:
+        print 'two list are not equal in line: ' + str(i)
+        continue
