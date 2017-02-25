37457e0c8b24973cb3f90ba8efc219621b56bb0c
Ajfaust <frostyflakes59@gmail.com>
2017-02-25 14:30:04 -0800

    Added additional statistics variables

--- Project2/ProbC.py
+++ Project2/ProbC.py
@@ -6,7 +6,7 @@ import random
 
 
 class Customer(Process):
-    def __init(self,a,b):
+    def __init(self, a, b):
         Process.__init__(self)
         self.Type = 'C'
         self.aplha = a
@@ -19,7 +19,7 @@ class Customer(Process):
             activate(S,S.Run())
 
 class Inventory(Process):
-    def __init__(self,a,b):
+    def __init__(self, a, b):
         Process.__init__(self)
         self.Type = 'I'
         self.alpha = a
@@ -32,26 +32,47 @@ class Inventory(Process):
             activate(S,S.Run())
 
 class Store(Process):
+    # Current stock
     stock = 0
+    # Total number of customers
     numCust = 0
+    # Total amount of inventory deliveries
+    invDeliveries = 0
+    # Total time customers have waited
     custWait = 0
+    # Number of orders filled immediately
+    servedImmediately = 0
+    # Number of inventory deliveries served to customers immediately
+    deliveryToCust = 0
+    # List to represent customers waiting
     waiting = []
-    def __init__(self,typ,time):
+    def __init__(self, typ, time):
         Process.__init__(self)
         self.Type = typ
         self.time = time
     def Run(self):
+        # Serve Customer
         if self.Type = 'C':
-            # Serve Customer
+            # If there is stock, serve customer immediately
             if stock:
+                servedImmediately += 1
                 stock -= 1
+            # If there is no stock, add customer to waiting list
             else:
                 # 1 is just a placeholder to show there is a customer waiting
                 waiting.append(1)
+            # Increment number of customers
+            numCust += 1
+
+        # Update inventory
         elif self.Type = 'I':
-            # Update inventory
+            # If ther is no one in the waiting list, increment stock
             if not waiting:
                 stock += 1
+            # If there are people in the waiting list, make the waiting list one
+            # shorter and add waiting time
             else:
                 waiting = [1:]
                 custWait += self.time
+                deliveryToCust += 1
+            invDeliveries += 1
