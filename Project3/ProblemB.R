d0f1be31aaf96b10b29dd5947f8a7663a9fa3350
Ajfaust <frostyflakes59@gmail.com>
2017-03-04 13:08:56 -0800

    Used arg instead of result
    We were using result to add up arg, and then adding it to the end anyway, so why not just add as we go.

--- Project3/ProblemB.R
+++ Project3/ProblemB.R
@@ -3,31 +3,29 @@ walk <- function(currdir, f, arg, firstcall = TRUE) {
   # Keep track of the starting directory
   startdir <- getwd()
   # Switch to the directory specified in the function
-  
+
   setwd(currdir)
-  
-  result <- arg
-  
+
   # Get list of all files
   filelist <- character()
-  
+
   # For each file in the list...
   for (fi in dir()) {
     if (file.info(fi)$isdir) {
-      result <- result + walk(fi, f, arg, FALSE)
+      arg + walk(fi, f, arg, FALSE)
     } else{
       # Otherwise, file is a file, so add it to the file list
       filelist <- c(filelist, fi)
     }
   }
-  
+
   #call the f function
-  result <- result + f(currdir, filelist, arg)
-  
-  
+  arg + f(currdir, filelist, arg)
+
+
   # Switch back to starting directory
   setwd(startdir)
-  
+
   arg + result
 }
 
@@ -40,12 +38,12 @@ nfiles <- function(drname, filelist, arg) {
 #this will include the empty space character.
 nbyte <- function(drname, filelist, arg) {
   total.bytes <- 0
-  
+
   for (f in filelist) {
     total.bytes <- total.bytes  + file.info(f)$size
   }
   arg + total.bytes
-  
+
 }
 
 #remove all the empty directories.
