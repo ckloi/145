0e762be4a7993a170ebf3c12166ff301a617c00b
Ajfaust <frostyflakes59@gmail.com>
2017-03-04 17:54:32 -0800

    Added check for consecutive pixels
    Couldn't think of a shorter way off the top of my head. If you come up with one, feel free to implement it.

--- Project3/ProblemA.R
+++ Project3/ProblemA.R
@@ -42,7 +42,7 @@ secretencoder <- function(imgfilename,msg,startpix,stride,consec = NULL){
 
   if((ncol(pa) - startpix +1) * nrow(pa) < total.pixs.need){
     stop("Not enough space for the message!")
-  })
+  }
 
   # We only need to check for consectutive bits if consec is not NULL
   # Otherwise, we check for consec number of consecutive bits
@@ -58,6 +58,40 @@ secretencoder <- function(imgfilename,msg,startpix,stride,consec = NULL){
     #change the char to the destination pixel
     #print(a)
     printf("index is [%d,%d]",pa.row,columnpix)
+    # Check for adjacent pixels by comparing changed picture with original
+    #   (only if consec is not NULL)
+
+    if(!is.null(consec)){
+      # As long as adjacent >= consec, you must keep moving until you find a
+      #   spot that has < consec adjacent pixels.
+      while(1){
+        adjacent <- 0
+        # Check each pixel directly adjacent to current one (diagonals don't count)
+        if (pa[pa.row-1,columnpix] != imgfile[pa.row-1,columnpix]){
+          adjacent <- adjacent + 1
+        }
+        if (pa[pa.row+1,columnpix] != imgfile[pa.row+1,columnpix]){
+          adjacent <- adjacent + 1
+        }
+        if(pa[pa.row,columnpix-1] != imgfile[pa.row,columnpix-1]){
+          adjacent <- adjacent + 1
+        }
+        if(pa[pa.row,columnpix+1] != imgfile[pa.row,columnpix+1]){
+          adjacent <- adjacent + 1
+        }
+        # If there are not consec number of adjacent pixels, break
+        if(adjacent < consec){
+          break
+        }
+        # Otherwise, move stride pixels further
+        pa.row <- pa.row + stride
+        if(pa.row > nrow(pa)){
+          columnpix <- columnpix + 1
+          pa.row <- pa.row - nrow(pa)
+        }
+      }
+    }
+
     pa[pa.row,columnpix] <- utf8ToInt(a) / 128
 
     print(pa[pa.row,columnpix])
