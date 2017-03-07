cd8511f5912e1ea103b901b154ead8a25b370c66
Ajfaust <frostyflakes59@gmail.com>
2017-03-07 08:20:24 -0800

    Fixed bugs in ProblemA (I think)

--- Project3/ProblemA.R
+++ Project3/ProblemA.R
@@ -66,6 +66,8 @@ secretencoder <- function(imgfilename,msg,startpix,stride,consec = NULL){
       while(1){
         # Get position of pixels adjacent to the current pixel
         adjacent <- c(pixel+1,pixel-1,pixel+nrow(pa),pixel-nrow(pa)) %% length(pa)
+        # Avoids an index of 0 (mod is applied across all values in index)
+        adjacent <- ifelse(adjacent%%length(pa),adjacent%%length(pa),length(pa))
         # Create a T/F vector based on if positions are in check vector or not
         isadjacent <- adjacent %in% check
         # If the amount of Ts in check vector is less than consec, then no conflicts
@@ -75,18 +77,24 @@ secretencoder <- function(imgfilename,msg,startpix,stride,consec = NULL){
         }
         pixel <- pixel + stride
       }
+      index <- ifelse(pixel%%length(pa),pixel%%length(pa),length(pa))
       # Place value at current pixel, taking wrap around into account
-      pa[pixel %% length(pa)] <- value
+      pa[index] <- value
       # Add current pixel position to check vector
       check <- c(check, pixel)
     }
   }
 
   else{
-    pa[seq(startpix, length(values)*stride, stride) %% length(pa)] <- values
+    # Get indices to write to
+    indices <- seq(startpix, length(values)*stride, stride)
+    # Avoids indices being 0
+    indices <- ifelse(indices%%length(pa),indices%%length(pa),length(pa))
+    pa[indices] <- values
   }
   result <- imgfile
   result@grey <- pa
+  print(length(pa))
   return(result)
 
 }
@@ -112,7 +120,10 @@ secretdecoder <- function(imgfilename,startpix,stride,consec=NULL){
     # Read every 'stride'th pixel, and add it's corresponding utf value to message
     #   (rounding is needed as division is not always exact)
     while(pa[(pixel+stride) %% length(pa)] != 0){
-      message <- c(message, intToUtf8(round(pa[pixel <- pixel+stride %% length(pa)]*128)))
+      pixel <- pixel + stride
+      # Avoids index being 0
+      index <- ifelse(pixel%%length(pa),pixel%%length(pa),length(pa))
+      message <- c(message, intToUtf8(round(pa[index]*128)))
     }
   }
   else{
@@ -122,14 +133,18 @@ secretdecoder <- function(imgfilename,startpix,stride,consec=NULL){
     # Loop until null character is encountered
     while(pa[pixel %% length(pa)] != 0){
       # Get position of pixels adjacent to the current pixel
-      adjacent <- c(pixel+1,pixel-1,pixel+nrow(pa),pixel-nrow(pa)) %% length(pa)
+      adjacent <- c(pixel+1,pixel-1,pixel+nrow(pa),pixel-nrow(pa))
+      # Avoids index being 0
+      adjacent <- ifelse(adjacent%%length(pa),adjacent%%length(pa),length(pa))
       # Create a T/F vector based on if positions are in check vector or not
       isadjacent <- adjacent %in% check
       # If the amount of Ts in check vector is less than consec, then no conflicts
       #   (TRUE represents an adjacent pixel that has been written to)
       if(length(isadjacent[isadjacent==TRUE]) < consec){
+        # Avoids index being 0
+        index <- ifelse(pixel%%length(pa),pixel%%length(pa),length(pa))
         # Read current pixel and add it's corresponding utf value to message
-        message <- c(message, intToUtf8(round(pa[pixel %% length(pa)]*128)))
+        message <- c(message, intToUtf8(round(pa[index]*128)))
       }
       pixel <- pixel + stride
     }
@@ -139,8 +154,8 @@ secretdecoder <- function(imgfilename,startpix,stride,consec=NULL){
   return(message)
 }
 
-startpixel <- 456
-stride1 <- 4000
+startpixel <- 2
+stride1 <- 2
 consec <- NULL
-write.pnm(secretencoder("LLL.pgm","ABCDEFGHIJKLMNOPQRSTUVWXYZ",startpixel,stride1),'LLL1.pgm',4)
-print(secretdecoder("LLL1.pgm",startpixel,stride1,4))
+write.pnm(secretencoder("LLL.pgm","This sentence should be correct",startpixel,stride1),'LLL1.pgm',3)
+print(secretdecoder("LLL1.pgm",startpixel,stride1,3))
