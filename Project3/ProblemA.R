d0b2e0829b82391edd096ce84065805164543afd
Ajfaust <frostyflakes59@gmail.com>
2017-03-07 12:51:51 -0800

    Removed unnecessary code

--- Project3/ProblemA.R
+++ Project3/ProblemA.R
@@ -30,22 +30,13 @@ secretencoder <- function(imgfilename,msg,startpix,stride,consec = NULL){
     warning("Stride is not relatively prime to image size. Overwriting may occur.")
   }
   if(length(msg) > length(pa)){
-    stop("Message is too long to place in this image.")
+    stop("Not enough space for the message.")
   }
 
   # Appropriate numeric values that will be added to the picture, with 0 at the
   #   end to represent the end of the message
   values <- utf8ToInt(msg)/128
   values <- c(values,0.0)
-
-  # if((ncol(pa) - startpix +1) * nrow(pa) < total.pixs.need){
-  #   stop("Not enough space for the message!")
-  # }
-
-  #now we start to embed the message.
-
-  # Check for adjacent pixels by comparing changed picture with original
-  #   (only if consec is not NULL)
 
   if(!is.null(consec)){
     # Vector that contains all positions that have been written to
@@ -65,9 +56,6 @@ secretencoder <- function(imgfilename,msg,startpix,stride,consec = NULL){
       }
       # Loop until position with no conflicts is found
       while(1){
-        if(count > 6000){
-          stop("Inifinite loop (maybe)")
-        }
         # Get position of pixels adjacent to the current pixel
         adjacent <- c(pixel+1,pixel-1,pixel+nrow(pa),pixel-nrow(pa))
         # Avoids an index of 0 (mod is applied across all values in index)
@@ -76,13 +64,12 @@ secretencoder <- function(imgfilename,msg,startpix,stride,consec = NULL){
         isadjacent <- adjacent %in% check
         # If the amount of Ts in check vector is less than consec, then no conflicts
         #   (TRUE represents an adjacent pixel that has been written to)
-        if(length(isadjacent[isadjacent==TRUE]) < consec){
+        if(length(isadjacent[isadjacent==TRUE]) <= consec){
           break
         }
         pixel <- pixel + stride
         # Avoids index being 0
         pixel <- modifyindex(pixel,pa)
-        count <- count + 1
       }
       # Place value at current pixel, taking wrap around into account
       pa[pixel] <- value
@@ -121,35 +108,29 @@ secretdecoder <- function(imgfilename,startpix,stride,consec=NULL){
   pixel <- startpix
   # Vector to hold all read characters
   message <- intToUtf8(round(pa[startpix]*128))
+
   if(is.null(consec)){
     pixel <- pixel + stride
     # Avoids index being 0
     pixel <- modifyindex(pixel,pa)
     count <- 0
     # Read every 'stride'th pixel, and add it's corresponding utf value to message
-    #   (rounding is needed as division is not always exact)
+    #   until null character is reached (rounding is needed as division is not
+    #   always exact)
     while(pa[pixel] != 0){
-      if(count > 6000){
-        stop("Stop from decder, consec == NULL")
-      }
       message <- c(message, intToUtf8(round(pa[pixel]*128)))
-      pixel <- pixel + stride
       # Avoids index being 0
-      pixel <- modifyindex(pixel,pa)
-      count <- 0
+      pixel <- modifyindex(pixel+stride,pa)
     }
   }
+
   else{
     # Vector containing all positions that have been read
     check <- startpix
-    pixel <- pixel + stride
-    pixel <- modifyindex(pixel,pa)
+    pixel <- modifyindex(pixel+stride,pa)
     count <- 0
     # Loop until null character is encountered
     while(pa[pixel] != 0){
-      if(count > 6000){
-        stop("Stop from decoder, consec != NULL")
-      }
       # Get position of pixels adjacent to the current pixel
       adjacent <- c(pixel+1,pixel-1,pixel+nrow(pa),pixel-nrow(pa))
       # Avoids index being 0
@@ -158,27 +139,26 @@ secretdecoder <- function(imgfilename,startpix,stride,consec=NULL){
       isadjacent <- adjacent %in% check
       # If the amount of Ts in check vector is less than consec, then no conflicts
       #   (TRUE represents an adjacent pixel that has been written to)
-      if(length(isadjacent[isadjacent==TRUE]) < consec){
+      if(length(isadjacent[isadjacent==TRUE]) <= consec){
         # Read current pixel and add it's corresponding utf value to message
         message <- c(message, intToUtf8(round(pa[pixel]*128)))
         check <- c(check,pixel)
       }
-      pixel <- pixel + stride
-      pixel <- modifyindex(pixel,pa)
-      count <- count + 1
+      pixel <- modifyindex(pixel+stride,pa)
     }
   }
+
   # Combine all read characters into one string
   message <- paste(message,collapse='')
   return(message)
 }
 
-startpixel <- 1234
-stride1 <- 5837
-consec <- NULL
+startpixel <- 6273
+stride1 <- 7163
+consec <- 3
 teststring <- "This is going to be a realy long sentence to test for any overwriting.
   If any overwriting occurs, the program should stop and you will not see this sentence.
   The file should be very small, so as to maximize the probability of overwriting occurring.
   If you see this paragraph, then the program works."
-write.pnm(secretencoder("LLL.pgm",teststring,startpixel,stride1),'LLL1.pgm')
-print(secretdecoder("LLL1.pgm",startpixel,stride1))
+write.pnm(secretencoder("small_test.pgm",teststring,startpixel,stride1,consec),'small_result.pgm')
+print(secretdecoder("small_result.pgm",startpixel,stride1,consec))
