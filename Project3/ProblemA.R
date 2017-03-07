59fad0d256e67c9c7399300406673c75deb758c2
Ajfaust <frostyflakes59@gmail.com>
2017-03-07 12:23:10 -0800

    Added stop in while loops to test for infinite loop
    Sometimes, the decoder loops infinitely when consec is not null

--- Project3/ProblemA.R
+++ Project3/ProblemA.R
@@ -29,6 +29,9 @@ secretencoder <- function(imgfilename,msg,startpix,stride,consec = NULL){
   if(stride%%length(pa) == 0){
     warning("Stride is not relatively prime to image size. Overwriting may occur.")
   }
+  if(length(msg) > length(pa)){
+    stop("Message is too long to place in this image.")
+  }
 
   # Appropriate numeric values that will be added to the picture, with 0 at the
   #   end to represent the end of the message
@@ -53,6 +56,7 @@ secretencoder <- function(imgfilename,msg,startpix,stride,consec = NULL){
     pixel <- startpix
     for(value in values){
       pixel <- pixel + stride
+      count <- 0
       # Avoids index being 0
       pixel <- modifyindex(pixel,pa)
       # Check if current pixel is going to be overwritten
@@ -61,6 +65,9 @@ secretencoder <- function(imgfilename,msg,startpix,stride,consec = NULL){
       }
       # Loop until position with no conflicts is found
       while(1){
+        if(count > 6000){
+          stop("Inifinite loop (maybe)")
+        }
         # Get position of pixels adjacent to the current pixel
         adjacent <- c(pixel+1,pixel-1,pixel+nrow(pa),pixel-nrow(pa))
         # Avoids an index of 0 (mod is applied across all values in index)
@@ -75,6 +82,7 @@ secretencoder <- function(imgfilename,msg,startpix,stride,consec = NULL){
         pixel <- pixel + stride
         # Avoids index being 0
         pixel <- modifyindex(pixel,pa)
+        count <- count + 1
       }
       # Place value at current pixel, taking wrap around into account
       pa[pixel] <- value
@@ -117,13 +125,18 @@ secretdecoder <- function(imgfilename,startpix,stride,consec=NULL){
     pixel <- pixel + stride
     # Avoids index being 0
     pixel <- modifyindex(pixel,pa)
+    count <- 0
     # Read every 'stride'th pixel, and add it's corresponding utf value to message
     #   (rounding is needed as division is not always exact)
     while(pa[pixel] != 0){
+      if(count > 6000){
+        stop("Stop from decder, consec == NULL")
+      }
       message <- c(message, intToUtf8(round(pa[pixel]*128)))
       pixel <- pixel + stride
       # Avoids index being 0
       pixel <- modifyindex(pixel,pa)
+      count <- 0
     }
   }
   else{
@@ -131,8 +144,12 @@ secretdecoder <- function(imgfilename,startpix,stride,consec=NULL){
     check <- startpix
     pixel <- pixel + stride
     pixel <- modifyindex(pixel,pa)
+    count <- 0
     # Loop until null character is encountered
     while(pa[pixel] != 0){
+      if(count > 6000){
+        stop("Stop from decoder, consec != NULL")
+      }
       # Get position of pixels adjacent to the current pixel
       adjacent <- c(pixel+1,pixel-1,pixel+nrow(pa),pixel-nrow(pa))
       # Avoids index being 0
@@ -144,9 +161,11 @@ secretdecoder <- function(imgfilename,startpix,stride,consec=NULL){
       if(length(isadjacent[isadjacent==TRUE]) < consec){
         # Read current pixel and add it's corresponding utf value to message
         message <- c(message, intToUtf8(round(pa[pixel]*128)))
+        check <- c(check,pixel)
       }
       pixel <- pixel + stride
       pixel <- modifyindex(pixel,pa)
+      count <- count + 1
     }
   }
   # Combine all read characters into one string
@@ -154,8 +173,12 @@ secretdecoder <- function(imgfilename,startpix,stride,consec=NULL){
   return(message)
 }
 
-startpixel <- 632
-stride1 <- 67326
+startpixel <- 1234
+stride1 <- 5837
 consec <- NULL
-write.pnm(secretencoder("LLL.pgm","This sentence should be correct",startpixel,stride1),'LLL1.pgm')
+teststring <- "This is going to be a realy long sentence to test for any overwriting.
+  If any overwriting occurs, the program should stop and you will not see this sentence.
+  The file should be very small, so as to maximize the probability of overwriting occurring.
+  If you see this paragraph, then the program works."
+write.pnm(secretencoder("LLL.pgm",teststring,startpixel,stride1),'LLL1.pgm')
 print(secretdecoder("LLL1.pgm",startpixel,stride1))
