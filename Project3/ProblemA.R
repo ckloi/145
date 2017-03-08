0a3423bccec8ae534927e482ae967c1203a6fce7
Ajfaust <frostyflakes59@gmail.com>
2017-03-07 21:03:06 -0800

    Tried to fix consec issue

--- Project3/ProblemA.R
+++ Project3/ProblemA.R
@@ -19,6 +19,7 @@ secretencoder <- function(imgfilename,msg,startpix,stride,consec = NULL){
   }
   #extract the pixel array
   pa <- imgfile@grey
+  original <- imgfile@grey
 
   if(length(pa)%%stride == 0){
     warning("Stride is not relatively prime to image size. Overwriting may occur.")
@@ -45,27 +46,38 @@ secretencoder <- function(imgfilename,msg,startpix,stride,consec = NULL){
     index <- startpix
     for (i in values){
 
-      # check surrounding with # of space in col and row
-      neighbors <- getNeighbors(index,pa)
-
       # counter <- 0
-      endIndex <- wrapAround(index,pa)
-
-      while (length(intersect(neighbors,pixAddress)) > consec  || index %in% pixAddress){
-
-        #wrap around
+      endIndex <- index
+      while(1){
+        sumUp <- checkUp(index,pixAddress)
+        sumCol <- checkDown(index,pixAddress,sumUp)
+        if(sumCol <= consec-1){
+          sumLeft <- checkLeft(index,pixAddress,nrow(pa))
+          sumRow <- checkRight(index,pixAddress,nrow(pa),sumLeft)
+          if(sumRow <= consec-1){
+            break
+          }
+        }
         index <- wrapAround(index+stride,pa)
-
-        neighbors <- getNeighbors(index,pa)
-
-        # counter <- counter + 1
-
-        if (index == endIndex){
-          # If you are here, you have gone through the entire image without writing
-          #   anything, and will continue to do so endlessly, so stop.
-          stop("cannot find place")
+        if(endIndex == index){
+          stop("Cannot find place")
         }
       }
+      # while (length(intersect(neighbors,pixAddress)) > 0 || index %in% pixAddress){
+      #
+      #   #wrap around
+      #   index <- wrapAround(index+stride,pa)
+      #
+      #   neighbors <- getNeighbors(index,pa,consec)
+      #
+      #   # counter <- counter + 1
+      #
+      #   if (index == endIndex){
+      #     # If you are here, you have gone through the entire image without writing
+      #     #   anything, and will continue to do so endlessly, so stop.
+      #     stop("cannot find place")
+      #   }
+      # }
       #wrap around
       # index <- wrapAround(index,xdim,ydim)
 
@@ -73,46 +85,7 @@ secretencoder <- function(imgfilename,msg,startpix,stride,consec = NULL){
       pixAddress <- c(pixAddress,index)
       index <- wrapAround(index+stride,pa)
     }
-    # Place rest of values in appropriate positions
-    # for(value in values[2:length(values)]){
-    #   # Avoids index being 0
-    #   pixel <- wrapAround(pixel+stride,pa)
-    #
-    #   # First pixel to start checking conflicts at
-    #   firstcheck <- pixel
-    #
-    #   # Loop until position with no conflicts is found
-    #   while(1){
-    #     # Get position of pixels adjacent to the current pixel
-    #     adjacent <- c(pixel+1,pixel-1,pixel+nrow(pa),pixel-nrow(pa))
-    #     # Avoids an index of 0 (mod is applied across all values in index)
-    #     adjacent <- wrapAround(adjacent,pa)
-    #     # Create a T/F vector based on if positions are in check vector or not
-    #     isadjacent <- adjacent %in% check
-    #     # If the amount of Ts in check vector is less than consec, then no conflicts
-    #     #   (TRUE represents an adjacent pixel that has been written to)
-    #     if(length(isadjacent[isadjacent==TRUE]) <= consec){
-    #       # If pixel will not be overwritten, break
-    #       if (!(pixel %in% check)){
-    #         break
-    #       }
-    #     }
-    #     # Avoids index being 0
-    #     pixel <- wrapAround(pixel+stride,pa)
-    #
-    #     if(pixel == firstcheck){
-    #       # If you are here, you have gone through the entire image without writing
-    #       #   anything, and will continue to do so endlessly, so stop.
-    #       stop("Endless conflicts")
-    #     }
-    #   }
-
-      # # Place value at current pixel, taking wrap around into account
-      # pa[pixel] <- value
-      # # Add current pixel position to check vector
-      # check <- c(check, pixel)
-    }
-
+  }
   result <- imgfile
   result@grey <- pa
   return(result)
@@ -132,12 +105,6 @@ secretdecoder <- function(imgfilename,startpix,stride,consec=NULL){
 
   imgfile <- read.pnm(imgfilename)
   pa <- imgfile@grey
-  # Current pixel
-  pixel <- startpix
-  # Vector to hold all read characters
-  message <- intToUtf8(round(pa[startpix]*128))
-  # Avoids index being 0
-  pixel <- wrapAround(pixel+stride,pa)
 
   if(is.null(consec)){
     # Get the appropriate indices to be read from. Start from startpix and
@@ -150,22 +117,27 @@ secretdecoder <- function(imgfilename,startpix,stride,consec=NULL){
   }
 
   else{
-    # Vector containing all positions that have been read
-    check <- startpix
-    count <- 0
-    # Loop until null character is encountered
-    while(pa[pixel] != 0){
-      neighbors <- getNeighbors(pixel,pa)
-      # Create a T/F vector based on if positions are in check vector or not
-      neighborsInCheck <- neighbors %in% check
-      # If the amount of Ts in check vector is less than consec, then no conflicts
-      #   (TRUE represents an adjacent pixel that has been written to)
-      if(length(neighborsInCheck[neighborsInCheck==TRUE]) <= consec){
-        # Read current pixel and add it's corresponding utf value to message
-        message <- c(message, intToUtf8(round(pa[pixel]*128)))
-        check <- c(check,pixel)
+    # Vector to hold all read characters
+    message <- vector(length=0)
+    # Vector to hold positions
+    pixAddress <- vector(length=0)
+    index <- startpix
+    while(pa[index] != 0){
+      while(1){
+        sumUp <- checkUp(index,pixAddress)
+        sumCol <- checkDown(index,pixAddress,sumUp)
+        if(sumCol <= consec-1){
+          sumLeft <- checkLeft(index,pixAddress,nrow(pa))
+          sumRow <- checkRight(index,pixAddress,nrow(pa),sumLeft)
+          if(sumRow <= consec-1){
+            break
+          }
+        }
+        index <- wrapAround(index+stride,pa)
       }
-      pixel <- wrapAround(pixel+stride,pa)
+      message <- c(message, intToUtf8(round(pa[index]*128)))
+      pixAddress <- c(pixAddress, index)
+      index <- wrapAround(index+stride,pa)
     }
   }
 
@@ -174,23 +146,68 @@ secretdecoder <- function(imgfilename,startpix,stride,consec=NULL){
   return(message)
 }
 
-# This function allows for wrap-around of matrix 'mat' and stops index from being 0
-#   (since result of mod could be 0, and R starts at 1 for indices)
-# wrapAround <- function(index,mat){
-#   return(ifelse(index%%length(mat),index%%length(mat),length(mat)))
-# }
+checkUp <- function(index,array,counter=0,i=1){
+  if(i > consec-1){
+    return(counter)
+  }
 
-# Returns the positon of all neighboring pixels of index
-getNeighbors <- function(index,mat){
-  maxIndex <- length(mat)
-
-  # Get bottom, top, right, and left neighbor positions, respectively
-  neighbors <- c(index + 1, index - 1, index + nrow(mat), index - nrow(mat)) %% maxIndex
-
-  # if the index is 0 , set that to max Index aka last index
-  neighbors[neighbors==0] <- maxIndex
-  return(neighbors)
+  if(index-i %in% array){
+    return(checkUp(index,array,counter+1,i+1))
+  }
+  return(counter)
 }
+
+checkDown <- function(index,array,counter,i=1){
+  if(i > consec-1){
+    return(counter)
+  }
+
+  if(index+i %in% array){
+    return(checkDown(index,array,counter+1,i+1))
+  }
+  return(counter)
+}
+
+checkLeft <- function(index,array,nrow,counter=0,i=1){
+  if(i > consec-1){
+    return(counter)
+  }
+
+  if(index-i*nrow %in% array){
+    return(checkLeft(index,array,nrow,counter+1,i+1))
+  }
+  return(counter)
+}
+
+checkRight <- function(index,array,nrow,counter=0,i=1){
+  if(i > consec-1){
+    return(counter)
+  }
+
+  if(index+i*nrow %in% array){
+    return(checkRight(index,array,nrow,counter+1,i+1))
+  }
+  return(counter)
+}
+# # Returns the positon of all neighboring pixels of index
+# getNeighbors <- function(index,mat,consec){
+#   neigbors <- vector(length=0)
+#
+#   maxIndex <- length(mat)
+#
+#   for (i in 1:consec){
+#       # Left and right pixels
+#       neigbors <- c(neigbors, (index + i*nrow(mat) ) %% maxIndex)
+#       neigbors <- c(neigbors, (index - i*nrow(mat) ) %% maxIndex)
+#
+#       # Top and bottom pixels
+#       neigbors <- c(neigbors, (index +  i ) %% maxIndex)
+#       neigbors <- c(neigbors, (index -  i ) %% maxIndex)
+#   }
+#   # if the index is 0 , set that to max Index aka last index
+#   neigbors[neigbors==0] <- maxIndex
+#   return(neigbors)
+# }
 
 # Modifies index to wrap around if needed
 wrapAround  <- function(index,mat){
@@ -200,33 +217,9 @@ wrapAround  <- function(index,mat){
   return(index)
 }
 
-startpixel <- 60000
-stride1 <- 512
-consec <- 3
-teststring <- "This is going to be a realy long sentence to test for any overwriting.
-  If any overwriting occurs, the program should stop and you will not see this sentence.
-  The file should be very small, so as to maximize the probability of overwriting occurring.
-  If you see this paragraph, then the program works. Making this longer in hopes of overwrite.
-  Like Reaaaaaaaaallllllllllllyyyyyyyyyyyyyy llllllloooooooooonnnnnnnnngggggggggg. So Long.
-  This will probably be the longest test string you've ever read. This is going to be longer
-  than a 152A lecture with Mukherjee. Yes, it's that long. That class is so boring. There
-  still isn't an overwrite yet. Wtf. Consec isn't null either. Crazy. I'll have to change the
-  values around. Still nothing. How long does this have to be to get an error? Like,for real.
-  I don't even know what to type anymore. I didn't thing the string would have to be this long
-  just to get a conflict. I might just have to resort to printing out a bunch of the same
-  character. Didn't think i'd have to write a novel for this. Still going. The picture is 256x256,
-  so rip this test string. Has to be like 60,000 characters in order to do anything. This is maybe
-  like a few hundred. Ok screw it. Here goes. aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-  abbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-  cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
-  cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
-  ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-  ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd.
-  FINALLY!"
+startpixel <- 45
+stride1 <- 63
+consec <- 4
+teststring <- "This is going to be a realy long sentence to test for any overwriting."
 write.pnm(secretencoder("small_test.pgm",teststring,startpixel,stride1,consec),'small_result.pgm')
 print(secretdecoder("small_result.pgm",startpixel,stride1,consec))
