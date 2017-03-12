bc5aef33623c105fcc612d342a9b8a730d5ac22b
ckloi <ckloi@ucdavis.edu>
2017-03-11 20:28:29 -0800

    add comments

--- Final/R/bintree.R
+++ Final/R/bintree.R
@@ -1,5 +1,6 @@
 
 
+
 bintree <- function() {
     arg <- list(tree = matrix(, ncol = 3))
     
@@ -14,7 +15,7 @@ push <- function(obj, value, row = 1) {
 
 push.bintree <- function(obj, value, row = 1) {
     
-    modifyMatrix <-  function (obj,row, col, value) {
+    modifyMatrix <-  function (obj, row, col, value) {
         newIndex <- nrow(obj$tree) + 1
         obj$tree[row, col] <- newIndex
         obj$tree <- rbind(obj$tree, c(NA, NA, NA))
@@ -26,21 +27,20 @@ push.bintree <- function(obj, value, row = 1) {
     # check whether first index is NA
     if (is.na(obj$tree[row, 1])) {
         obj$tree[row, 1] <- value
-        return(obj)
-    }
-    
-    
-    if (obj$tree[row, 1] > value) {
+        # check if first index is not NA, then check the value is less than to or greater than
+        # if first index is less than or equal to
+    } else if (value <= obj$tree[row, 1]) {
+        # if left child is na , set it
         if (is.na(obj$tree[row, 2])) {
-            obj <- modifyMatrix(obj,row, 2, value)
-            return(obj)
+            obj <- modifyMatrix(obj, row, 2, value)
         } else{
+            # if left child is not na, then find the index recusively
             obj <- push(obj, value, obj$tree[row, 2])
         }
-    } else if (obj$tree[row, 1] <= value) {
+        # if first index is greater tha, the similar approach as above
+    } else if (value > obj$tree[row, 1]) {
         if (is.na(obj$tree[row, 3])) {
-            obj <- modifyMatrix(obj,row, 3, value)
-            return(obj)
+            obj <- modifyMatrix(obj, row, 3, value)
         } else{
             obj <- push(obj, value, obj$tree[row, 3])
         }
@@ -57,7 +57,7 @@ t <- bintree()
 t <- push(t, 5)
 t <- push(t, 3)
 t <- push(t, 6)
-j <- push(j,2)
+j <- push(j, 2)
 t <- push(t, 2)
 t <- push(t, 10)
 t <- push(t, 11)
