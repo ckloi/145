aa29b7e0b8aac9f5fa58c154881f39270b56502a
Ajfaust <frostyflakes59@gmail.com>
2017-03-13 16:45:16 -0700

    Rewrote push and pop, made print put everything on one line.
    Just in case we need this version. It's also alot easier to read in my opinion.

--- Final/R/bintree.R
+++ Final/R/bintree.R
@@ -2,12 +2,10 @@
 
 
 bintree <- function() {
-    
-    arg <- list(tree = c(NA,NA,NA))
-    
-    attr(arg, "class") <- "bintree"
-    arg
-    
+  rtrn <- list()
+  rtrn$tree <- rbind(c(NA,NA,NA))
+  class(rtrn) <- "bintree"
+  return(rtrn)
 }
 
 
@@ -16,123 +14,61 @@ push <- function(obj, value, row = 1) {
 }
 
 push.bintree <- function(obj, value, row = 1) {
-    
-    modifyMatrix <-  function (obj, row, col, value) {
-        
-        newIndex <- NA
-        # if lower two row is not created , then create two new row
-        if (row + 2 > nrow(obj$tree)){
-            obj$tree <- rbind(obj$tree, c(NA, NA, NA))
-            obj$tree <- rbind(obj$tree, c(NA, NA, NA))
-        }
-        # search for unused row
-        # if left child , search from row + 1 to the end
-        # if right child, search from row + 2 to the end
-        for (i in  (row + col - 1):nrow(obj$tree)){
-            if (is.na(obj$tree[i,1])){
-                newIndex <- i
-                break
-            }
-        }
-        
-        # if cannot find unused row , then append to the end of the matrix
-        if (is.na(newIndex)){
-            newIndex <- nrow(obj$tree) + 1
-            #since we cannot find unusend row, we have to create new row at the end
-            obj$tree <- rbind(obj$tree, c(NA, NA, NA))
-        }
-        
-        
-        obj$tree[row, col] <- newIndex
-        obj$tree[newIndex, 1] <- value
-        return(obj)
+  # Check if first value is NA. If it is, place the value to be pushed in column 1
+  #   Only triggers on first push
+  if(is.na(obj$tree[1,1])){
+    obj$tree[1,1] <- value
+    return(obj)
+  }
+
+  nextIndex <- nrow(obj$tree) + 1
+  # If value to insert is less than current value, go left until left pointer is
+  #   NA
+  if(value <= obj$tree[row,1]) {
+    # If left pointer is NA, insert value at next row, with pointers of NA
+    if(is.na(obj$tree[row,2])) {
+      obj$tree[row,2] <- nextIndex
+      obj$tree <- rbind(obj$tree, c(value,NA,NA))
+      return(obj)
     }
-    
-    
-    # check whether head is NA
-    if (is.na(obj$tree[1])) {
-        # first case : new tree -> one row with NA NA NA
-        # second case : empty tree with all unused rows
-        # if everything and head is delete, then we have a empty matrix with n rows,
-        # so we have to reset it to one row before turning vector to matrix
-        
-        # if dim is not null, then it is second case, since vector's dim is null
-        if (!is.null(dim(obj$tree))){
-            obj$tree <- obj$tree[1,]
-        }
-        # turn vector to 1 x 3 matrix
-        dim(obj$tree) <- c(1,3)
-        obj$tree[1,1] <- value
-        return(obj)
+    # Otherwise recursively call on left node to find the right index
+    row <- obj$tree[row,2]
+    return(push(obj,value,row))
+  }
+
+  if(value > obj$tree[row,1]) {
+    if(is.na(obj$tree[row,3])) {
+      obj$tree[row,3] <- nextIndex
+      obj$tree <- rbind(obj$tree, c(value,NA,NA))
+      return(obj)
     }
-    
-    # if value is less than or equal to the first index
-    if (value <= obj$tree[row, 1]) {
-        # if left child is na , set it
-        if (is.na(obj$tree[row, 2])) {
-            obj <- modifyMatrix(obj, row, 2, value)
-        } else{
-            # if left child is not na, then find the appropriate row recusively
-            obj <- push(obj, value, obj$tree[row, 2])
-        }
-        # if value is greater than first index, the similar approach as above
-    } else if (value > obj$tree[row, 1]) {
-        if (is.na(obj$tree[row, 3])) {
-            obj <- modifyMatrix(obj, row, 3, value)
-        } else{
-            obj <- push(obj, value, obj$tree[row, 3])
-        }
-    }
-    
-    return (obj)
-    
+    row <- obj$tree[row,3]
+    return(push(obj,value,row))
+  }
+  return(obj)
 }
 
-pop <- function(obj,row = 1,crow = 1) {
+pop <- function(obj,row = 1) {
     UseMethod("pop", obj)
 }
 
-pop.bintree <- function(obj,row = 1,crow = 1){
-    
-    #there is no more left child
-    if(is.na(obj$tree[row,2])){
-        
-        obj$tree[row,1]  <- NA
-        
-        if (row == 1){
-            if (!is.na(obj$tree[row,3])){
-                rightchildIndex  <- obj$tree[row,3]
-                obj$tree[row,] <- obj$tree[rightchildIndex,]
-                obj$tree[rightchildIndex,] <- NA
-                return(obj)
-            }
-            
-        }
-        
-        
-        if(!is.na(obj$tree[row,3])){
-            obj$tree[crow,2] <- obj$tree[row,3]
-            obj$tree[row, 3] <- NA
-            
-        }else{
-            
-            obj$tree[crow,2] <- NA
-        }
-        #obj$tree <- rbind(obj$tree,c(NA,NA,NA))
-    }else{
-        #there is left chile
-        #current row of the node that have left child
-        crow <- row
-        obj <- pop(obj,obj$tree[row,2],crow)
-    }
+pop.bintree <- function(obj,row = 1){
+  # Check if head is the leftmost node
+  if(is.na(obj$tree[1,2])){
+    right <- obj$tree[row,3]
+    obj$tree[row,] <- obj$tree[right,]
+    return (obj)
+  }
+
+  # Look ahead one node to the left. If that node's left pointer is NA, pop that
+  #   node (change left pointer of current node to right node of child node)
+  left <- obj$tree[row,2]
+  if(is.na(obj$tree[left,2])){
+    obj$tree[row,2] <- obj$tree[left,3]
     return(obj)
-    
-}
+  }
 
-
-
-print <- function(obj){
-    UseMethod("print", obj)
+  return(pop(obj,left))
 }
 
 
@@ -142,14 +78,20 @@ print.bintree <- function(obj, row=1){
     if(!is.na(left)){
         print.bintree(obj,left)
     }
-    
+
     # Print your value
-    print(obj$tree[row,1])
-    
+    if(!is.na(obj$tree[row,1])){
+      cat(obj$tree[row,1], " ")
+    }
+
     # Print right subtree
     right <- obj$tree[row,3]
     if(!is.na(right)){
         print.bintree(obj,right)
+    }
+    # Print newline once done
+    if(row == 1){
+      cat('\n')
     }
 }
 
@@ -161,7 +103,7 @@ print("Pushing 4")
 t <- push(t, 4)
 t <- pop(t)
 print("Popping (2 should be gone)")
-print(t$tree)
+print(t)
 print("Pushing 10")
 t <- push(t, 10)
 print("Pushing 7")
@@ -172,30 +114,29 @@ print("Pushing 11")
 t <- push(t, 11)
 print("Popping (1 should be gone)")
 t <- pop(t)
-print(t$tree)
+print(t)
 print("Pushing 100")
 t <- push(t, 100)
 print("Popping (4 should be gone)")
 t <- pop(t)
-print(t$tree)
+print(t)
 print("Popping (7 should be gone)")
 t <- pop(t)
-print(t$tree)
+print(t)
 print("Popping (10 should be gone, 11 should be head)")
 t <- pop(t)
-print(t$tree)
+print(t)
 print("Pushing 2")
 t <- push(t,2)
 print("Popping (2 should be gone)")
 t <- pop(t)
-print(t$tree)
+print(t)
 print("Popping (11 should be gone, 100 should be head)")
 t <- pop(t)
-print(t$tree)
+print(t)
 print("Popping (100 should be gone)")
 t <- pop(t)
-print(t$tree)
+print(t)
 print("Pushing 2")
 t <- push(t,2)
-print(t$tree)
-
+print(t)
